aa0535b601a8f2b2264c9191a9453fbb482b137d
DAM 1 <DAM 1@DESKTOP-006C3VV>
2024-01-16 08:53:04 +0100

    Agregar archivos de proyecto.

---
+++ GestionNominas_v1/Nomina.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionNominas_v1
+{
+    public class Nomina
+    {
+
+    }
+}
